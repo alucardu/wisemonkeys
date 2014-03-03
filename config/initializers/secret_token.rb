96aef88405fe9137aff0b79f90406db09ad8883a
Your Name <you@example.com>
2014-03-03 12:09:30 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wisemonkeys::Application.config.secret_key_base = 'd0787e80b981495622ef1732522bc148ed92c881b4b16c849e72fea5b86fd5bf50361978c9719f54b388ff0292f4f223ce8060c3d82e06c689dc9c55c5be85e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wisemonkeys::Application.config.session_store :cookie_store, key: '_wisemonkeys_session'
