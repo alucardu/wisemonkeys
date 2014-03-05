21c5ee66ba7cb4093619c24b6c57423d89d3276e
Your Name <you@example.com>
2014-03-05 13:53:52 +0100

    writing tests for new page

--- spec/requests/wisemonkeys_spec.rb
+++ spec/requests/wisemonkeys_spec.rb
@@ -13,9 +13,15 @@ subject { page }
   describe "Upload page" do
   	before {visit pictures_path}
   	it {should have_content("Kunst Uploaden")}
-
+  	it {should have_link("Klik om kunst te uploaden")}
   end
 
+  describe "New page" do
+  	before {visit pictures_new_path}
+  	it {should have_content("Picture Upload")}
+  	it {should have_content("Title")}
+  	it {should have_content("Description")}
+  end
 
 
 end
