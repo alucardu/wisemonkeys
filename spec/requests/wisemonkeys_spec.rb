ac310f48234eaea80645cf1bc6c5bc8bafa4e94e
Your Name <you@example.com>
2014-03-05 14:32:19 +0100

    writing tests for creating an art item

--- spec/requests/wisemonkeys_spec.rb
+++ spec/requests/wisemonkeys_spec.rb
@@ -23,5 +23,15 @@ subject { page }
   	it {should have_content("Description")}
   end
 
+  it "create an art item" do
+    visit '/pictures/new'
+    fill_in 'Title', :with => 'kunst'
+    fill_in 'Description', :with => 'Dit is mooie kunst'
+    click_button 'Upload Picture'
+    expect(page).to have_content 'kunst'
+    expect(page).to have_content 'Dit is mooie kunst'
+  end
+
+
 
 end
