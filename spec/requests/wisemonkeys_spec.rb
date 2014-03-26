f72541b333ef5fa92c35f5820577a9a616af32a6
Chris van der Meer <cvandermeer@live.nl>
2014-03-26 11:47:36 +0100

    adding a test for a doe mee button on the form and vote content on the homepage

--- spec/requests/wisemonkeys_spec.rb
+++ spec/requests/wisemonkeys_spec.rb
@@ -1,29 +1,31 @@
 require 'spec_helper'
 
-describe "WiseMonkeys" do
+describe 'WiseMonkeys' do
 
 subject { page }
 
-  describe "Homepage" do
-    it "cannot be loaded" do
+  describe 'Homepage' do
+    it 'cannot be loaded' do
       get root_path
     end
+    it {should have_content('vote')}
   end
 
-  describe "Upload page" do
-  	before {visit pictures_path}
-  	it {should have_content("Kunst Uploaden")}
-  	it {should have_link("Klik om kunst te uploaden")}
+  describe 'Upload page' do
+    before {visit pictures_path}
+    it {should have_content('Kunst Uploaden')}
+    it {should have_link('Klik om kunst te uploaden')}
   end
 
-  describe "New page" do
-  	before {visit pictures_new_path}
-  	it {should have_content("Wedstrijden")}
-  	it {should have_content("Titel")}
-  	it {should have_content("Omschrijving")}
+  describe 'New page' do
+    before {visit pictures_new_path}
+    it {should have_content('Wedstrijden')}
+    it {should have_content('Titel')}
+    it {should have_content('Omschrijving')}
+    it {should have_button('Doe mee')}
   end
 
-  it "create an art item" do
+  it 'create an art item' do
     visit '/pictures/new'
     fill_in 'Title', :with => 'kunst'
     fill_in 'Description', :with => 'Dit is mooie kunst'
