8540ad7f8f891df277a2435fcb25d2ae30ddfd27
Chris van der Meer <cvandermeer@live.nl>
2014-04-09 11:06:52 +0200

    writing new rspec for uploading within a competition

--- spec/feature/competitions_spec.rb
+++ spec/feature/competitions_spec.rb
@@ -21,12 +21,42 @@ feature 'Competitions' do
    	end
 
    	click_button 'Wedstrijd aanmaken'
+   
+
 	  visit competitions_path
 	  expect(page).to have_content ('Kunstwedstrijd')
 	  expect(page).to have_content ('Dit is een Kunstwedstrijd')
 	  expect(page).to have_content ('500 euro')
 	  expect(page).to have_content ('2014-06-05 00:00:00 UTC')
 	end
+
+	scenario 'Creating a competition and uploading a file' do
+
+		visit competitions_new_path
+		expect(page).to have_content ('Wedstrijd aanmaken')
+
+		within ('#new_competition') do
+			fill_in 'competition_title', with: 'Kunstwedstrijd'
+      fill_in 'competition_description', with: 'Dit is een Kunstwedstrijd'
+      fill_in 'competition_prize', with: '500 euro'
+      fill_in 'competition_deadline', with: '5 juni'
+   	end
+
+   	click_button 'Wedstrijd aanmaken'
+		click_link 'Klik om mee te doen!'
+
+   	fill_in 'picture_title', with: 'kunst'
+    fill_in 'picture_description', with: 'Dit is mooie kunst'
+    attach_file 'picture_image', File.join(Rails.root, 'spec', 'images', 'art.jpg')
+
+    click_button 'Doe mee'
+    visit competitions_path
+
+    click_link 'Kunstwedstrijd'
+
+    expect(page).to have_content 'kunst'
+    expect(page).to have_content 'Dit is mooie kunst'
+  end 
 end
 
 
