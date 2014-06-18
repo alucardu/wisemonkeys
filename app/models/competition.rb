9a013f70a3c5607dd8d38370fddde8402b0edb72
Chris van der Meer <cvandermeer@live.nl>
2014-06-18 12:53:56 +0200

    removing conflicted line

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,5 +2,4 @@ class Competition < ActiveRecord::Base
 	validates_presence_of :title, :description, :prize, :deadline, :deadline_signin
 	has_many :pictures
 	belongs_to :user
-	validates_presence_of :title, :description, :prize
 end
