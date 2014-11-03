b3d3a58b2d4a4604da38a227b47f0a066e33279a
Peter Boomsma <peter@no-illusions.nl>
2014-11-03 15:41:45 +0100

    added sqlite3 for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,43 @@
 # config valid only for Capistrano 3.1
-set :rbenv_ruby, '2.1.1'
+lock '3.1.0'
+
 set :application, 'wisemonkeys'
 set :repo_url, 'git@github.com:cvandermeer/wisemonkeys.git'
 
-set :scm, :git
+set :deploy_to, '/home/deploy/wisemonkeys'
 
-set :format, :pretty
-set :log_level, :info
+set :linked_files, %w{config/database.yml secrets.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-set :linked_dirs, %w{tmp/pids tmp/sockets log public/uploads}
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-set :default_env, { path: "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
-set :keep_releases, 5
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
 
-#set :normalize_asset_timestamps, false
-set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
+# Default value for :scm is :git
+# set :scm, :git
 
-set :migration_role, 'postgres' # Defaults to 'db'
+# Default value for :format is :pretty
+# set :format, :pretty
 
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
 
 namespace :deploy do
 
@@ -28,14 +48,7 @@ namespace :deploy do
     end
   end
 
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+  
 end
